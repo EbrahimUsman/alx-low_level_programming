f7f0634f85c136345584c162f9ba50bb383d8201
EbrahimUsman <ebrahim19usman@gmail.com>
2022-06-02 01:00:05 -0700

    simple prints a message specified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-/*starting of main method 
-*this is program that simple prints the message specified */
+/*
+ *starting of main method 
+*this is program that simple prints the message specified
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
