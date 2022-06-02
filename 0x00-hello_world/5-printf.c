265b47585d2b7ac5a9f32a3c90e2dbdb8af76abc
EbrahimUsman <ebrahim19usman@gmail.com>
2022-06-02 01:18:43 -0700

    prints message using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+include <stdio.h>
+/**
+ * main - use printf method to rint message
+ * Retun: returns 0 if exit correctly else non-zero 
+ */
+int main(void)
+{
+printf("%s%,"with proper grammar, but the outcome is a piece of art\n");
+return (0);
+}
+
