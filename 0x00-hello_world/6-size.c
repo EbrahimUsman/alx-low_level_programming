596f2dfca293543dd9ccfd6943b4caf01b15434e
EbrahimUsman <ebrahim19usman@gmail.com>
2022-06-02 02:03:25 -0700

    prints size of data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - prints the size of various data types
- * Return: 0 if excited correctly, non-zero otherwise
+	* main - prints the size of various data types
+	* Return: 0 if excited correctly, non-zero otherwise
  */
 int main(void)
 {
