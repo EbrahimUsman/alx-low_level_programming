73f210974f012f1b55c609ec763041548275acfe
EbrahimUsman <ebrahim19usman@gmail.com>
2022-06-02 01:29:37 -0700

    prints message using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - use printf method to rint message
- * Return: 0 if exit correctly else non-zero 
+ * Return: 0 if exit correctly else non-zero
  */
 int main(void)
 {
