e6252d155a84f82f299a150ad04768fe297721af
EbrahimUsman <ebrahim19usman@gmail.com>
2022-06-02 00:53:10 -0700

    generates assembly code and save

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/* starting of main method */
+
+/* starting of main method 
+ this is program that simple prints the message specified
+  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
