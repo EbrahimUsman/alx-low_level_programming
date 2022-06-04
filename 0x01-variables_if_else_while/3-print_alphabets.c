afc5d597bf6f007319a2854b84cbfc38f0a3a792
EbrahimUsman <ebrahim19usman@gmail.com>
2022-06-03 23:35:46 -0700

    commit message

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * main - prints characters of upper case and lower case
+ * main - prints characters of upper ca
+ * se and lower case
  * Return: prints 0 if runs correctly none-zero
- * */
+ */
 int main(void)
 {
 	char m = 'a';
 
-	for(m = 'a'; m <= 'z'; m++)
+	for (m = 'a'; m <= 'z'; m++)
 		putchar(m);
-
-	for(m = 'Z'; m >= 'A'; m--)
-		
+	for (m = 'Z'; m <= 'A'; m++)
 		putchar(m);
-		
 	putchar('\n');
 	printf("\n");
 	return (0);
